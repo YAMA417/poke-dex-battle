26d6dda9fc80f19682111371ffd1df3e4a008fe0
YAMA417 <yamanaka.mm417@gmail.com>
2025-11-23 10:46:53 +0900

    feat: nodeなどを最新バージョンに修正、またnpm run devで個別に起動できるように修正

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         // ポケモンカラー
         pokemon: {
-          blue: '#3B4CCA',
-          yellow: '#FFDE00',
-          red: '#FF0000',
+          blue: "#3B4CCA",
+          yellow: "#FFDE00",
+          red: "#FF0000",
         },
       },
     },
